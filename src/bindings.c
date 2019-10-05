a4e6e8d5ff0440665a935d3635fc870504cfc0fc
CrossR <r.cross@lancaster.ac.uk>
2019-10-05 12:56:55 +0100

    Initial commit.

---
+++ src/bindings.c
@@ -0,0 +1,14 @@
+#include <caml/alloc.h>
+#include <caml/bigarray.h>
+#include <caml/callback.h>
+#include <caml/custom.h>
+#include <caml/memory.h>
+#include <caml/mlvalues.h>
+#include <caml/threads.h>
+
+#include <choices.h>
+
+void temp() {
+    printf("Hello!");
+};
+
